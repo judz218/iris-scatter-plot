550bdd87eddf4e169055fe9db8f8069b0a6294a8
hase218 <chju23041@g.nihon-u.ac.jp>
2025-05-29 18:39:22 +0900

    selectつけた

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 function App() {
   return (
     <div>
-      <header>
-        <h1>scatter plot of iris data</h1>
+      <header className="hero is-info is-small ">
+        <div className="hero-body"> 
+            <h1 className="has-text-white">scatter plot of iris data</h1>
+        </div>
       </header>
+      <main>
+        <div>
+          <label for="x-select">x property</label>
+          <select name="x" id="x property">
+            <option value="sepal length">sepal length</option>
+            <option value="sepal width">sepal width</option>
+            <option value="petal length">petal length</option>
+            <option value="petal width">petal width</option>
+          </select>
+        </div>
+        <div>
+          <label for="y-select">y property</label>
+          <select name="y" id="y property">
+            <option value="sepal length">sepal length</option>
+            <option value="sepal width">sepal width</option>
+            <option value="petal length">petal length</option>
+            <option value="petal width">petal width</option>
+          </select>
+        </div>
+        <div>
+          
+        </div>
+
+      </main>
     </div>
   );
 }
