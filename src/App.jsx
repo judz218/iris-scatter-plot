fcd261647e06c90e19e0fd153006273e1cf84422
hase218 <chju23041@g.nihon-u.ac.jp>
2025-05-30 20:45:19 +0900

    コントロールをコンポーネント化してみた

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Controls from "./components/Controls"
 
 import {useState, useEffect} from "react";
 
@@ -17,24 +18,7 @@ function App() {
   return (
     <div>
       <Header />
-        <div>
-          <label id="x-select">x property</label>
-          <select value={xProperty} onChange={(event)=> {setXProperty(event.target.value)}}>
-            <option value="sepal length">sepal length</option>
-            <option value="sepal width">sepal width</option>
-            <option value="petal length">petal length</option>
-            <option value="petal width">petal width</option>
-          </select>
-        </div>
-        <div>
-          <label id="y-select">y property</label>
-          <select value={yProperty} onChange={(event)=> {setYProperty(event.target.value)}}>
-            <option value="sepal length">sepal length</option>
-            <option value="sepal width">sepal width</option>
-            <option value="petal length">petal length</option>
-            <option value="petal width">petal width</option>
-          </select>
-        </div>
+      <Controls />
         <div>
           content
         </div>
--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,12 +1,13 @@
 import {useState, useEffect} from "react";
 
 
-export default function Control() {
+export default function Controls() {
     const [xProperty, setXProperty] = useState("sepal lebgth");
     const [yProperty, setYProperty] = useState("sepal width");
     return (
-        <div>
-          <label for="x-select">x property</label>
+      <div>
+      <div>
+          <label id="x-select">x property</label>
           <select value={xProperty} onChange={(event)=> {setXProperty(event.target.value)}}>
             <option value="sepal length">sepal length</option>
             <option value="sepal width">sepal width</option>
@@ -14,5 +15,15 @@ export default function Control() {
             <option value="petal width">petal width</option>
           </select>
         </div>
+        <div>
+          <label id="y-select">y property</label>
+          <select value={yProperty} onChange={(event)=> {setYProperty(event.target.value)}}>
+            <option value="sepal length">sepal length</option>
+            <option value="sepal width">sepal width</option>
+            <option value="petal length">petal length</option>
+            <option value="petal width">petal width</option>
+          </select>
+        </div>
+      </div>
     );
 }
