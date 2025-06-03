e9d704ad9c1ef222a09ad8ac6aaa800409c8a79e
hase218 <chju23041@g.nihon-u.ac.jp>
2025-06-04 02:57:47 +0900

    gpt曰く、要素の追加はReact のJSXでやった方がいいらしい

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,6 +1,50 @@
-// いったんxは"sepal length"、yは"sepal width"に固定して散布図をつくろう
-import * as d3 from "d3";
-import { useEffect } from "react";
+// // いったんxは"sepal length"、yは"sepal width"に固定して散布図をつくろう
+// import * as d3 from "d3";
+// import { useEffect } from "react";
+
+// export default function Contents ({data, xProperty, yProperty}) {
+//     console.log(xProperty)
+//     const colors = {
+//         setosa: "green",
+//         versicolor: "purple",
+//         virginica: "orange"
+//     };
+//     const w = 800;
+//     const h = 800;
+
+//     const r = 5;
+
+//     useEffect(() => {
+//         var svg = d3.select("body")
+//                 .append("svg")
+//                 .attr("width", w)
+//                 .attr("height", h);
+
+//         const xScale = d3.scaleLinear()
+//                         .domain([0, d3.max(data, d => d[xProperty])])
+//                         .range([0, w]);
+        
+//         const yScale = d3.scaleLinear()
+//                         .domain([0, d3.max(data, d => d[yProperty])])
+//                         .range([h, 0]);
+
+//         const circles = svg.selectAll("circle")
+//                             .data(data)
+//                             .enter()
+//                             .append("circle");
+        
+//         circles.attr("cx", d => xScale(d[xProperty]))
+//                 .attr("cy", d => yScale(d[yProperty]))
+//                 .attr("r", r)
+//                 .attr("fill", d => colors[d.species]);
+
+//     }, [data, xProperty, yProperty]);
+    
+
+//     return (
+//         <></>
+//     );
+// }
 
 export default function Contents ({data, xProperty, yProperty}) {
     const colors = {
@@ -10,38 +54,27 @@ export default function Contents ({data, xProperty, yProperty}) {
     };
     const w = 800;
     const h = 800;
-
     const r = 5;
 
-    useEffect(() => {
-
-        var svg = d3.select("body")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h);
-
-        const xScale = d3.scaleLinear()
-                        .domain([0, d3.max(data, d => d.sepalLength)])
-                        .range([0, w]);
-        
-        const yScale = d3.scaleLinear()
-                        .domain([0, d3.max(data, d => d.sepalWidth)])
-                        .range([h, 0]);
-
-        const circles = svg.selectAll("circle")
-                            .data(data)
-                            .enter()
-                            .append("circle");
-        
-        circles.attr("cx", d => xScale(d.sepalLength))
-                .attr("cy", d => yScale(d.sepalWidth))
-                .attr("r", r)
-                .attr("fill", d => colors[d.species]);
-
-    }, [data, xProperty, yProperty]);
+    const xScale = d3.scaleLinear()
+                     .domain([0, d3.max(data, d => d[xProperty])])
+                     .range([0, w]);
     
+    const yScale = d3.scaleLinear()
+                     .domain([0, d3.max(data, d => d[yProperty])])
+                     .range([h, 0]);
 
     return (
-        <></>
+        <svg width={w} height={h}>
+            {data.map((d, i) => (
+                <circle 
+                    key={i}
+                    cx={xScale(d[xProperty])}
+                    cy={yScale(d[yProperty])}
+                    r={r}
+                    fill={colors[d.species]}
+                />
+            ))}
+        </svg>
     );
 }
