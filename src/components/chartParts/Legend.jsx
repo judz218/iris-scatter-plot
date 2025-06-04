17d487a17a9abdb156b967ba4f7aedc39719d333
hase218 <chju23041@g.nihon-u.ac.jp>
2025-06-05 00:52:45 +0900

    Legendつくった

--- src/components/chartParts/Legend.jsx
+++ src/components/chartParts/Legend.jsx
@@ -1,5 +1,13 @@
 export default function Legend({colors}) {
+    const wh = 15;
     return (
-        <></>
+        <g transform="translate(650, 50)">
+            {Object.entries(colors).map(([species, color], i) => (
+                <g key={species} transform={`translate(0, ${i*20})`}>
+                    <rect x="0" y="0" width={wh} height={wh} fill={color}/>
+                    <text x={wh+4} y="0">{species}</text>
+                </g>
+            ))}
+        </g>
     );
 }
