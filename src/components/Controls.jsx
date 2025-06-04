69ab884880ba18663a9116e83686660e2997dd8d
hase218 <chju23041@g.nihon-u.ac.jp>
2025-06-05 02:03:22 +0900

    整理

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,24 +1,24 @@
 export default function Controls({ xProperty, yProperty, setXProperty, setYProperty }) {
   const propertyName = ["sepal length", "sepal width", "petal length", "petal width"];
   const propertyVal = ["sepalLength", "sepalWidth", "petalLength", "petalWidth"];
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <label id="x-select">x property</label>
-          <select value={xProperty} onChange={(event)=> {setXProperty(event.target.value)}}>
-            {propertyName.map((p, i) => (
-              <option key={i} value={propertyVal[i]}>{p}</option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label id="y-select">y property</label>
-          <select value={yProperty} onChange={(event)=> {setYProperty(event.target.value)}}>
-            {propertyName.map((p, i) => (
-              <option key={i} value={propertyVal[i]}>{p}</option>
-            ))}
-          </select>
-        </div>
+        <label id="x-select">x property</label>
+        <select value={xProperty} onChange={(event)=> {setXProperty(event.target.value)}}>
+          {propertyName.map((p, i) => (
+            <option key={i} value={propertyVal[i]}>{p}</option>
+          ))}
+        </select>
       </div>
-    );
+      <div>
+        <label id="y-select">y property</label>
+        <select value={yProperty} onChange={(event)=> {setYProperty(event.target.value)}}>
+          {propertyName.map((p, i) => (
+            <option key={i} value={propertyVal[i]}>{p}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 }
--- src/components/chartParts/ScatterPlot.jsx
+++ src/components/chartParts/ScatterPlot.jsx
@@ -1,8 +1,5 @@
 export default function ScatterPlot({data, xProperty, yProperty, xScale, yScale, colors}) {
-    
-
     const r = 5;
-
     return (
         <g>
             {data.map((d, i) => (
