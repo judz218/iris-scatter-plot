3c6fb8a15349dae4e73a5a4439b6e277c775eed3
hase218 <chju23041@g.nihon-u.ac.jp>
2025-06-05 01:46:09 +0900

    x軸のメモリつくった

--- src/components/chartParts/XAxis.jsx
+++ src/components/chartParts/XAxis.jsx
@@ -1,14 +1,22 @@
 export default function XAxis({xScale, h, padding}) {
-    const ticks = xScale.ticks(12);
+    const ticks = xScale.ticks(8);
     return (
         <g>
-            <line
+            <g>
+                <line
                 x1={xScale.range()[0]}
                 y1={h - padding}
                 x2={xScale.range()[1]}
                 y2={h - padding}
                 stroke="black"
             />
+            </g>
+            {ticks.map((tick, i) => (
+                <g key={i} transform={`translate(${xScale(tick)}, ${h - padding})`}>
+                    <line x1="0" y1="0" x2="0" y2="5" stroke="black"/>
+                    <text x="0" y="5" dominantBaseline="hanging" textAnchor="middle">{tick}</text>
+                </g>
+            ))}
         </g>
     );
 }
