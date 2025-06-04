150c1546ab482916a0760c2f406e16e51724c43e
hase218 <chju23041@g.nihon-u.ac.jp>
2025-06-05 01:33:33 +0900

    x軸ひいた

--- src/components/chartParts/XAxis.jsx
+++ src/components/chartParts/XAxis.jsx
@@ -1,5 +1,14 @@
 export default function XAxis({xScale, h, padding}) {
+    const ticks = xScale.ticks(12);
     return (
-        <></>
+        <g>
+            <line
+                x1={xScale.range()[0]}
+                y1={h - padding}
+                x2={xScale.range()[1]}
+                y2={h - padding}
+                stroke="black"
+            />
+        </g>
     );
 }
