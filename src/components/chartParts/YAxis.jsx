981575e1a784c5f217146cbc2e6efbfee4631382
hase218 <chju23041@g.nihon-u.ac.jp>
2025-06-05 02:00:56 +0900

    y軸つけた

--- src/components/chartParts/YAxis.jsx
+++ src/components/chartParts/YAxis.jsx
@@ -1,5 +1,22 @@
 export default function YAxis({yScale, padding}) {
+    const ticks = yScale.ticks(12);
     return (
-        <></>
+        <g>
+            <g>
+                <line
+                x1={padding}
+                y1={yScale.range()[0]}
+                x2={padding}
+                y2={yScale.range()[1]}
+                stroke="black"
+            />
+            </g>
+            {ticks.map((tick, i) => (
+                <g key={i} transform={`translate(${padding}, ${yScale(tick)})`}>
+                    <line x1="-5" y1="0" x2="0" y2="0" stroke="black"/>
+                    <text x="-10" y="0" dominantBaseline="middle" textAnchor="end">{tick}</text>
+                </g>
+            ))}
+        </g>
     );
 }
