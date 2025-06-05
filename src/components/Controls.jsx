924bd7fafa218e1251bbb5d1acfbe0e23c28f18d
hase218 <chju23041@g.nihon-u.ac.jp>
2025-06-05 11:20:11 +0900

    blumaで遊んだ

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,22 +2,27 @@ export default function Controls({ xProperty, yProperty, setXProperty, setYPrope
   const propertyName = ["sepal length", "sepal width", "petal length", "petal width"];
   const propertyVal = ["sepalLength", "sepalWidth", "petalLength", "petalWidth"];
   return (
-    <div>
+    <div className="notification" >
       <div>
-        <label id="x-select">x property</label>
-        <select value={xProperty} onChange={(event)=> {setXProperty(event.target.value)}}>
+        <label className="label"id="x-select">x property</label>
+        <div className="select">
+          <select value={xProperty} onChange={(event)=> {setXProperty(event.target.value)}}>
           {propertyName.map((p, i) => (
             <option key={i} value={propertyVal[i]}>{p}</option>
           ))}
-        </select>
+          </select>
+        </div>
       </div>
-      <div>
-        <label id="y-select">y property</label>
-        <select value={yProperty} onChange={(event)=> {setYProperty(event.target.value)}}>
+
+      <div>  
+        <label className="label" id="y-select">y property</label>
+        <div className="select">
+          <select value={yProperty} onChange={(event)=> {setYProperty(event.target.value)}}>
           {propertyName.map((p, i) => (
             <option key={i} value={propertyVal[i]}>{p}</option>
           ))}
-        </select>
+          </select>
+        </div>
       </div>
     </div>
   );
